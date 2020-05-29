50e887be6a1a8af23f6d5ea2ccb1f6617a1c074b
t-asano <14119184+t-asano@users.noreply.github.com>
2020-05-30 04:04:57 +0900

    Fix comment

--- media_dater.ps1
+++ media_dater.ps1
@@ -1,5 +1,5 @@
-﻿#
-# Media Dater v1.0
+#
+# Media Dater
 # 画像/動画ファイルの撮影日時情報をもとにファイルの名前/日時を変更する
 #
 # 動作環境:
@@ -10,13 +10,13 @@
 # 対応ファイル:
 #   拡張子がjpg/mov/mp4の画像/動画ファイル
 #
-# 実行方法:
-#   1. PowerShellを開く
-#   2. 画像/動画ファイルが存在するフォルダへ移動する
+# 使い方:
+#   1. このファイル(media_dater.ps1)を適当な場所へ配置
+#   2. 画像/動画ファイルが存在するフォルダ上でPowerShellを開く
 #   3. media_dater.ps1 を実行する
 #
 # 参考:
-#   https://neos21.hatenablog.com/entry/2019/11/11/080000#日付文字列を整形してリネームする
+#   https://neos21.hatenablog.com/entry/2019/11/11/080000
 #   https://qiita.com/Kosen-amai/items/52ec7e4e2f15f6a09bc3
 #   https://qiita.com/kmr_hryk/items/882b4851e23cec607e70
 #
